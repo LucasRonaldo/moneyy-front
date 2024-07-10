1204f8497a1cf56cade9aadf7f98679ad3d77ab3
LucasRonaldo <Lucasronaldoalvesdasilva@gmail.com>
2024-07-10 01:15:23 -0300

    atualizado

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import {
+    BrowserRouter,
+    Route,
+    Routes
+} from "react-router-dom";
+
+
+
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+           
+               
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRouter;
